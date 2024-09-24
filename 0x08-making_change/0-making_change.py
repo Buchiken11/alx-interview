b1d12518769c646f43f3999abcf40dfbe0eebcbe
Buchiken11 <buchiken025@gmail.com>
2024-09-24 03:39:26 +0100

    modifield

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,23 +1,26 @@
 #!/usr/bin/python3
 """
-fewest number of coins needed to
-meet a given amount total
+greegy algorithms: the coin Change problem
 """
 
 
 def makeChange(coins, total):
-    """ fewest number of coins needed to meet total """
+    """Determines the fewest number of coins needed to meet a given
+    amount total when given a pile of coins of different values.
+    """
     if total <= 0:
         return 0
-    # sort the coins from top tp bottom
-    coins.sort(reverse=True)
-    change = 0
-    for coin in coins:
-        if total <= 0:
-            break
-        sum_total = total
-        bal += sum_total
-        total -= (sum_total * coin)
-    if total != 0:
-        return -1
-    return change
+    counted = total
+    coins_counted = 0
+    coin_idx = 0
+    sorted_coins = sorted(coins, reverse=True)
+    n = len(coins)
+    while counted > 0:
+        if coin_idx >= n:
+            return -1
+        if counted - sorted_coins[coin_idx] >= 0:
+            counted -= sorted_coins[coin_idx]
+            coins_counted += 1
+        else:
+            coin_idx += 1
+    return coins_counted
